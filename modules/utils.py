4dd1440add93f8a39ff34512844a50ab2a02d9e3
Maks Mr1necs <46063054+mr1necs@users.noreply.github.com>
2025-07-01 13:19:55 +0300

    Create utils.py

---
+++ modules/utils.py
@@ -0,0 +1,41 @@
+from argparse import ArgumentParser
+
+
+def get_arguments() -> dict[str, str]:
+    """
+    Parse command-line arguments for the application.
+
+    Returns:
+        dict[str, str]: Dictionary of parsed command-line arguments.
+    """
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        default="cvtest.avi",
+        help="Path to input video (e.g. cvtest.avi)",
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        default="yolo11n.pt",
+        help="Path to load YOLO model (or YOLO model name (e.g. yolo11n.pt))",
+    )
+    parser.add_argument(
+        "-d",
+        "--device",
+        type=str,
+        default="mps",
+        help="Device to use: 'mps', 'cuda', or 'cpu'",
+    )
+    parser.add_argument(
+        "-s",
+        "--show",
+        action="store_true",
+        default=True,
+        help="Show video on screen (default: False)",
+    )
+    return vars(parser.parse_args())
